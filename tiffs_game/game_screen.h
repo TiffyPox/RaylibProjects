248d8b42ed3019d740e3606bf85ef46110e491f0
TiffyPox <tiffanyjaynedavies@hotmail.co.uk>
2025-01-23 16:10:30 +0000

    update main

---
+++ tiffs_game/game_screen.h
@@ -0,0 +1,13 @@
+#ifndef GAME_SCREEN_H
+#define GAME_SCREEN_H
+
+// Define the GameScreen enum
+typedef enum GameScreen 
+{
+    LOGO = 0,
+    TITLE,
+    GAMEPLAY,
+    ENDING
+} GameScreen;
+
+#endif // GAME_SCREEN_H
--- tiffs_game/main.cpp
+++ tiffs_game/main.cpp
@@ -1,38 +1,123 @@
 #include <raylib/raylib.h>
+#include "game_screen.h"
+
+void UpdateLogo(int &framesCounter, GameScreen &currentScreen);
+void UpdateTitle(GameScreen &currentScreen);
+void UpdateGameplay(GameScreen &currentScreen);
+void UpdateEnding(GameScreen &currentScreen);
+
+void DrawLogo();
+void DrawTitle();
+void DrawGameplay();
+void DrawEnding();
+
+const int screenWidth = 1920;
+const int screenHeight = 1200;
 
 // Main entry point
 int main(void)
 {
     // Initialization
-    const int screenWidth = 800;
-    const int screenHeight = 400;
 
-    InitWindow(screenWidth, screenHeight, "Tiff's Game");
+    InitWindow(screenWidth, screenHeight, "Tiffany's Game");
+
+    GameScreen currentScreen = LOGO;
+
+    int framesCounter = 0;
 
     SetTargetFPS(60);
 
-    const char* text = ("Hello Dan");
-
     // Main game loop
+    while (!WindowShouldClose())
     {
-        while(!WindowShouldClose())
+        // Update
+        switch (currentScreen)
         {
-            // Update
-
-            // Draw
-            BeginDrawing();
-
-                ClearBackground(SKYBLUE);
-
-                DrawText(text, (float)screenWidth /2 - 150, (float)screenHeight / 2 - 50, 60, DARKBLUE);
-
-            EndDrawing();
+            case LOGO: UpdateLogo(framesCounter, currentScreen); break;
+            case TITLE: UpdateTitle(currentScreen); break;
+            case GAMEPLAY: UpdateGameplay(currentScreen); break;
+            case ENDING: UpdateEnding(currentScreen); break;
+            default: break;
         }
+
+        // Draw
+        BeginDrawing();
+        ClearBackground(SKYBLUE);
+
+        switch(currentScreen)
+        {
+            case LOGO: DrawLogo(); break;
+            case TITLE: DrawTitle(); break;
+            case GAMEPLAY: DrawGameplay(); break;
+            case ENDING: DrawEnding(); break;
+            default: break;
+        }
+
+        EndDrawing();
     }
 
     // De-Initialization
-
     CloseWindow();
 
     return 0;
+}
+
+void UpdateLogo(int &framesCounter, GameScreen &currentScreen)
+{
+    framesCounter++;
+    if (framesCounter > 120) // Wait for 2 seconds
+    {
+        currentScreen = TITLE;
+    }
+}
+
+void UpdateTitle(GameScreen &currentScreen)
+{
+    if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
+    {
+        currentScreen = GAMEPLAY;
+    }    
+}
+
+void UpdateGameplay(GameScreen &currentScreen)
+{
+    if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
+    {
+        currentScreen = ENDING;
+    }
+}
+
+void UpdateEnding(GameScreen &currentScreen)
+{
+    if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
+    {
+        currentScreen = TITLE;
+    }
+}
+
+void DrawLogo()
+{
+    DrawText("LOGO SCREEN", 20, 20, 40, SKYBLUE);
+    DrawText("WAIT for 2 seconds...", 290, 220, 40, BLACK);
+}
+
+void DrawTitle()
+{
+    DrawRectangle(0, 0, screenWidth, screenHeight, GREEN);
+    DrawText("TITLE SCREEN", 20, 20, 40, BLACK);
+    DrawText("Press ENTER or TAP to jump to GAMEPLAY SCREEN", 120, 220, 40, BLACK);
+}
+
+void DrawGameplay()
+{
+    DrawRectangle(0, 0, screenWidth, screenHeight, PURPLE);
+    DrawText("GAMEPLAY SCREEN", 20, 20, 40, BLACK);
+    DrawText("Press ENTER or TAP to jump to ENDING SCREEN", 130, 220, 40, BLACK);
+}
+
+void DrawEnding()
+{
+    DrawRectangle(0, 0, screenWidth, screenHeight, BLUE);
+    DrawText("ENDING SCREEN", 20, 20, 40, BLACK);
+    DrawText("Press ENTER or TAP to jump to TITLE SCREEN", 120, 220, 40, BLACK);
 }
